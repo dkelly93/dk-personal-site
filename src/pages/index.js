9004da29d6a4419b3c311cd660452de0d4c18463
David Kelly <celtic_dave@hotmail.co.uk>
2019-07-23 21:49:36 +0100

    Adding content

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "David Kelly"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -15,19 +15,18 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+        <img style={{ marginLeft: 'auto', marginRight: 'auto', borderRadius: '50%' , width: '50%'}} src="./dave.png" alt="DKProfile" />
         <h1>
-          Hey people{" "}
+          Hey {" "}
           <span role="img" aria-label="wave emoji">
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
+        <p>I'm David, a Software Engineer currently working for Liberty IT in Belfast</p>
         <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
+          Apart from loving all things Javascript, I love playing and watching football and most of all spending time with my beautiful Fiancé and Pomeranian Marla!
         </p>
-        <p>Now go build something great!</p>
+        <p>Check out my blog for some random snippets and brain dumps</p>
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
